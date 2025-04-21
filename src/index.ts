54a6108de63505d4908ccff520a30268b6084beb
Martirossyan Ararat <myfailemtions@gmail.com>
2025-04-21 09:32:18 +0200

    feat: registered customer controller

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { json } from "express";
 import cors from "cors";
 import "dotenv/config";
+import customerController from './modules/customer/customer.controller'
 
 const app = express();
+
+app.use('/customers', customerController);
 
 app.use(cors());
 app.use(json());
